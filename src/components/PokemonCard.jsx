5fb2058ffa5616b6959cd74136418d7fefda5ae5
CYRIELLE THOMAS <cyriellethomas.pro@gmail.com>
2023-03-28 12:16:49 +0200

    React quête 03

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,14 +1,3 @@
-function PokemonCard() {
-  return ( 
-<figure>
-    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
-         alt="bulbasaur"></img>
-    <figcaption>bulbasaur</figcaption>
-</figure>
-)
-const pokemon =pokemonList[0]; 
-
-}
 
 const pokemonList = [
   {
@@ -18,6 +7,20 @@ const pokemonList = [
   },
   {
     name: "mew",
+    
   },
 ];
+
+function PokemonCard() {
+
+    const pokemon = pokemonList[1];
+
+    return (
+        <figure>
+          {pokemon.imgSrc ? (<img src={pokemon.imgSrc} alt={pokemon.name}/>) : (<p>???</p>)}
+          <figcaption>{pokemon.name}</figcaption>
+        </figure>
+      )
+    }
+
 export default PokemonCard;
