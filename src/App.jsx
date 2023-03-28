db09a37ae4c1033b866e7cf163277f46c6f897f3
CYRIELLE THOMAS <cyriellethomas.pro@gmail.com>
2023-03-28 10:40:15 +0200

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import PokemonCard from "./components/PokemonCard";
 
-
-
 function App() {
   return (
     <div>
       <PokemonCard />
     </div>
-  );
+  )
 }
 
 export default App;
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,10 +1,23 @@
 function PokemonCard() {
-  return
+  return ( 
 <figure>
     <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
          alt="bulbasaur"></img>
     <figcaption>bulbasaur</figcaption>
 </figure>
+)
+const pokemon =pokemonList[0]; 
+
 }
 
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
 export default PokemonCard;
