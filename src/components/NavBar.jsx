4172c3137f5beb971d5883d9f8ab0010b1b2d9c5
CYRIELLE THOMAS <cyriellethomas.pro@gmail.com>
2023-03-29 17:29:22 +0200

    Quête 08

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,19 +3,33 @@
 
 
 
-function NavBar({pokemonIndex, setPokemonIndex, pokemonList}) {
+function NavBar({pokemonIndex, pokemonList, setPokemonIndex}) {
 
-  const handleClickNext = () => {
-    setPokemonIndex(pokemonIndex+1)
-  }
-  const handleClickBack = () => {
-   setPokemonIndex(pokemonIndex-1)
-  }
-  return (
-    <div>
-{ (pokemonIndex > 0) ? (<button onClick={handleClickBack}>Back</button>): ""}
-{(pokemonIndex < pokemonList.length - 1) ? (<button onClick={handleClickNext}>Next</button>) : "" }
-</div>
+  const changePokemon = ((pokemonIndex) =>{
+    setPokemonIndex(pokemonIndex); 
+    
+  }); 
+
+  return(
+  <ul>
+    {pokemonList.map((pokemon, pokemonIndex) => ( 
+    <button key={pokemon.name} onClick={() => changePokemon(pokemonIndex)}>{pokemon.name}</button>  
+  ))}
+  </ul>
  )
 }
  export default NavBar;
+
+
+//  const handleClickNext = () => {
+//   setPokemonIndex(pokemonIndex+1)
+// }
+// const handleClickBack = () => {
+//  setPokemonIndex(pokemonIndex-1)
+// }
+// return (
+//   <div>
+// { (pokemonIndex > 0) ? (<button onClick={handleClickBack}>Back</button>): ""}
+// {(pokemonIndex < pokemonList.length - 1) ? (<button onClick={handleClickNext}>Next</button>) : "" }
+// </div>
+// )
