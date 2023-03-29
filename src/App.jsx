6df6cd3ba5d01c87dbb877c041de033f5a536b2e
CYRIELLE THOMAS <cyriellethomas.pro@gmail.com>
2023-03-29 11:59:05 +0200

    State 1 quête 06

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,53 @@
+
 import PokemonCard from "./components/PokemonCard";
-import PropTypes from "prop-types";
+import { useState } from "react";
 
 const pokemonList = [
   {
-    name: "bulbasaur",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "mew",
-    
-  },
-];
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "charmander",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+    },
+    {
+      name: "squirtle",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+    },
+    {
+      name: "pikachu",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
+  // let pokemonIndex=pokemonList[0]; 
 
-PokemonCard.PropTypes = {
-  pokemonList: PropTypes.shape ({
-  name:PropTypes.string.isRequired,
-  imgSrc : PropTypes.string
-}),
-}
+  // const [pokemonIndex, setPokemonIndex] = useState(pokemonList[0]); 
+  // const handleClickNext = () => {
+  //   setPokemonIndex(indexPokemon+1)
+  // }
 
 function App() {
+  const [pokemonIndex, setPokemonIndex] = useState(0); 
+  
+  const handleClickNext = () => {
+     setPokemonIndex(pokemonIndex+1)
+  }
+  const handleClickBack = () => {
+    setPokemonIndex(pokemonIndex-1)
+  }
   return (
     <div>
-      <PokemonCard pokemon={pokemonList[0]} />
+      <PokemonCard pokemon={pokemonList[pokemonIndex]} />
+      {(pokemonIndex > 0) ?(<button onClick={handleClickBack}>Back</button>) : ""}
+      {(pokemonIndex < pokemonList.length - 1) ? (<button onClick={handleClickNext}>Next</button>) : "" } 
     </div>
   )
 }
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,13 +1,21 @@
+import PropTypes from "prop-types";
 
 
+PokemonCard.PropTypes = {
+  pokemonList: PropTypes.shape ({
+  name:PropTypes.string.isRequired,
+  imgSrc : PropTypes.string
+}),
+}
 
-function PokemonCard({pokemon}) {
-    return (
-        <figure>
-          {pokemon.imgSrc ? (<img src={pokemon.imgSrc} alt={pokemon.name}/>) : (<p>???</p>)}
-          <figcaption>{pokemon.name}</figcaption>
-        </figure>
-      )
+
+function PokemonCard({pokemon}) {  
+  return (
+    <figure>
+      {pokemon.imgSrc ? (<img src={pokemon.imgSrc} alt={pokemon.name}/>) : (<p>???</p>)}
+      <figcaption>{pokemon.name}</figcaption>
+    </figure>
+  )      
     }
 
 export default PokemonCard;
